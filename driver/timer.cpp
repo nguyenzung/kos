db369589005fa9e471837b45becac6d94374017a
Murt Meiv <85049064+murtmeiv@users.noreply.github.com>
2022-09-27 08:58:20 -0400

    Update timer.cpp
    Dùng CMOS để có được thời gian,

--- driver/timer.cpp
+++ driver/timer.cpp
@@ -1,6 +1,17 @@
 #include <driver/timer.h>
 #include <kernel/interruptmanager.h>
 #include <kernel/printer.h>
+#define CURRENT_YEAR        2022                            // Change this each year!                                                                 
+#define CURRENT_MONTH       01
+
+int century_register = 0x00;                                // Set by ACPI table parsing code if possible                                             
+
+unsigned char second;
+unsigned char minute;
+unsigned char hour;
+unsigned char day;
+unsigned char month;
+unsigned int year;
 
 using namespace driver;
 
@@ -15,7 +26,96 @@ Timer::~Timer() {
 }
 
 void Timer::active() {
+  //printf(" Time ===========");                                                                                                                      
+      unsigned char century;
+      unsigned char last_second;
+      unsigned char last_minute;
+      unsigned char last_hour;
+      unsigned char last_day;
+      unsigned char last_month;
+      unsigned char last_year;
+      unsigned char last_century;
+      unsigned char registerB;
+ 
+      // Note: This uses the "read registers until you get the same values twice in a row" technique
+      //       to avoid getting dodgy/inconsistent values due to RTC updates
+ 
+      while (get_update_in_progress_flag());                // Make sure an update isn't in progress
+      second = get_RTC_register(0x00);
+      minute = get_RTC_register(0x02);
+      hour = get_RTC_register(0x04);
+      day = get_RTC_register(0x07);
+      month = get_RTC_register(0x08);
+      year = get_RTC_register(0x09);
+      if(century_register != 0) {
+            century = get_RTC_register(century_register);
+      }
+ 
+      do {
+            last_second = second;
+            last_minute = minute;
+            last_hour = hour;
+            last_day = day;
+            last_month = month;
+            last_year = year;
+            last_century = century;
+ 
+            while (get_update_in_progress_flag());           // Make sure an update isn't in progress
+            second = get_RTC_register(0x00);
+            minute = get_RTC_register(0x02);
+            hour = get_RTC_register(0x04);
+            day = get_RTC_register(0x07);
+            month = get_RTC_register(0x08);
+            year = get_RTC_register(0x09);
+            if(century_register != 0) {
+                  century = get_RTC_register(century_register);
+            }
+      } while( (last_second != second) || (last_minute != minute) || (last_hour != hour) ||
+               (last_day != day) || (last_month != month) || (last_year != year) ||
+               (last_century != century) );
+ 
+      registerB = get_RTC_register(0x0B);
+ 
+      // Convert BCD to binary values if necessary
+ 
+      if (!(registerB & 0x04)) {
+            second = (second & 0x0F) + ((second / 16) * 10);
+            minute = (minute & 0x0F) + ((minute / 16) * 10);
+            hour = ( (hour & 0x0F) + (((hour & 0x70) / 16) * 10) ) | (hour & 0x80);
+            day = (day & 0x0F) + ((day / 16) * 10);
+            month = (month & 0x0F) + ((month / 16) * 10);
+            year = (year & 0x0F) + ((year / 16) * 10);
+            if(century_register != 0) {
+                  century = (century & 0x0F) + ((century / 16) * 10);
+            }
+      }
+ 
+      // Convert 12 hour clock to 24 hour clock if necessary
+ 
+      if (!(registerB & 0x02) && (hour & 0x80)) {
+            hour = ((hour & 0x7F) + 12) % 24;
+      }
+ 
+      // Calculate the full (4-digit) year
+ 
+      if(century_register != 0) {
+            year += century * 100;
+      } else {
+            year += (CURRENT_YEAR / 100) * 100;
+            if(year < CURRENT_YEAR) year += 100;
+      }
+      printf("\nTime: ");
+      printf(" %d ",month);
+      printf("/");
+      printf(" %d ",day);
+      printf("/");
+      printf(" %d ",year);
+      printf("    ");
+      printf(" %d ",hour);
+      printf(":");
+      printf(" %d ",minute);
 
+    
 }
 
 void Timer::deactive() {
@@ -24,4 +124,14 @@ void Timer::deactive() {
 
 void Timer::handleInterrupt() {
     // Printer::println(" Timer INT ", 11);
+}
+
+int Timer::get_update_in_progress_flag() {
+      outb(0x70, 0x0A);
+      return (inb(0x71) & 0x80);
+}
+
+unsigned char Timer::get_RTC_register(int reg) {
+      outb(0x70, reg);
+      return inb(0x71);
 }
